33ce2d538cfff85f7f7712ac03c41d412594e602
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-06 21:11:42 -0300

    feat(produto): cria componente de produto

---
+++ src/app/components/produto/produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  imports: [],
+  templateUrl: './produto.html',
+  styleUrl: './produto.css',
+})
+export class Produto {}
