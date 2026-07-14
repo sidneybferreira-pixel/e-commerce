5a9a0b1973c4b29ae25fb7d8089a8b1152f78151
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-13 21:42:46 -0300

    feat :  src/app/components/produto/produto.ts

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [ UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+//'Cria a classe Produto com as propriedades produto, 
+export class Produto {
+  produto = 'notebook gamer'
+  preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+produtos =[
+  {nome:'Teclado', preco:49.99},
+  {nome:'Mouse', preco:29.99},
+  {nome:'Monitor', preco:149.99}
+];
+}
