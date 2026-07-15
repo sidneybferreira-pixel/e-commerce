0f2ad46ff58e4d5d1be5ddfb4fce1dbc904250a9
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-15 19:13:05 -0300

    feat  src/app/components/lista-produtos/lista-produtos.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { LiataProdutos } from './components/liata-produtos/liata-produtos';
+import { from } from 'rxjs';
 //import { RouterOutlet } from '@angular/router'; remove a importação do RouterOutle
-import {Produto} from './components/produto/produto';// importando a classe
+//import {Produto} from './components/produto/produto';// importando a classe
+import(LiataProdutos) from './components/liata-produtos/liata-produto';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [LiataProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Produto } from '../produto/produto';
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  produtos = [
+    {nome:'Teclado', preco:229.99},
+    {nome: 'Mouse', preco: 129.99}
+    {nome: 'Monito', preco: 2000}
+    {nome: 'Desktop', preco: 4999.99}
+    {nome: 'Headset', preco: 500}
+  ];
+}
