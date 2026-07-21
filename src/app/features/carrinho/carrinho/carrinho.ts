1742b330d8a4246476ea4ffb14e6adaefe09ae8d
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-21 18:48:43 -0300

    src/app/features/

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //! Lógica de carrinho virá na próxima aulas
+  //! Por ora, apenas exibe a página carrinho
+}
