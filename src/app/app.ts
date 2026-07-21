e458f047b2ee46793fa269c8382959d7b9ccbf21
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-21 19:59:29 -0300

    refactor(user): src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { ListaProdutos } from './components/produtos/lista-produtos/lista-produtos';
-import { from } from 'rxjs';
-//import { RouterOutlet } from '@angular/router'; remove a importação do RouterOutle
-//import {Produto} from './components/produto/produto';// importando a classe
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Mercado Liso';
 }
