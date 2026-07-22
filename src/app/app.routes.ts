b98a24144943e2bd9e1e308f23af5999c24062b1
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-22 18:49:42 -0300

    refactor: src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,24 @@
 import { Routes } from '@angular/router';
 import { ListaProdutos } from './components/produtos/lista-produtos/lista-produtos';
 import { Carrinho } from './features/carrinho/carrinho/carrinho';
+import { Home } from './features/home/home/home';
 
 export const routes: Routes = [
     {
         path:'',
-        component: ListaProdutos,
+        component: Home,
 
+    },
+    {
+        path:'produtos',
+        component: ListaProdutos,
     },
     {
         path:'carrinho',
         component: Carrinho,
+    },
+    {
+        path:'**',
+        redirectTo:'',
     },
 ];
