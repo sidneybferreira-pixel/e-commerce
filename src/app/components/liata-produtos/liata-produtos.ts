c5af4269c882e7bc2a7e15389d538b6b218bb7fd
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-22 21:33:17 -0300

    refector: src/app/components/liata-produtos/liata-produtos.ts

--- src/app/components/liata-produtos/liata-produtos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-liata-produtos',
-  imports: [],
-  templateUrl: './liata-produtos.html',
-  styleUrl: './liata-produtos.css',
-})
-export class LiataProdutos {}
