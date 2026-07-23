db7dceba728535534f0671bf1f8532f1bdc64e01
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-22 21:30:08 -0300

    refector:  src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,25 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './components/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
-import { Home } from './features/home/home/home';
+import { Routes} from "@angular/router";
+import { authGuard } from "./core/auth.guard";
 
 export const routes: Routes = [
     {
-        path:'',
-        component: Home,
-
+        path:'', //! router para raiz localhost:4200/
+        loadComponent: () =>
+            import('./features/home/home/home')
+        .then((m) => m.Home),
     },
     {
-        path:'produtos',
-        component: ListaProdutos,
+         path:'produtos',
+        loadComponent: () =>
+            import('./features/produtos/lista-produtos/lista-produtos')
+        .then((m) => m.ListaProdutos)
     },
     {
-        path:'carrinho',
-        component: Carrinho,
+           path:'carrinho',
+           canActivate: [authGuard],
+        loadComponent: () =>
+            import('./features/carrinho/carrinho/carrinho')
+        .then((m) => m.Carrinho)
     },
     {
         path:'**',
