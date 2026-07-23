2ee6df3a0ccdca55b0a0220a0f5ec913830a174e
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-07-22 21:40:13 -0300

    refector:  src/app/components/produtos/produtos.ts

--- src/app/components/produtos/produtos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-produtos',
-  imports: [],
-  templateUrl: './produtos.html',
-  styleUrl: './produtos.css',
-})
-export class Produtos {}
