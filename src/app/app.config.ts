d475729613261b2e14d7f33f0ebdd76e3f8157d5
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-03 21:08:01 -0300

    refactor: src/app/app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch, } from '@angular/common/http';
 import { routes } from './app.routes';
+import { withInterceptors } from '@angular/common/http';
+import { HttpInterceptor} from './core/interceptors/http.interceptor'
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch(),
+  withInterceptors([HttpInterceptor])),
+  ],
 };
