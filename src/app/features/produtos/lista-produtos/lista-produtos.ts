4753598e33860f5db558d89f186794e3c2fe294a
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-03 21:09:58 -0300

    refactor:src/app/features/produtos/lista-produtos/lista-produtos.ts

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -5,6 +5,8 @@ import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
+import { produtosService } from '../ptodutos.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -13,19 +15,14 @@ import { UpperCasePipe } from '@angular/common';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  //!Lista com dados - Array
-  produtos = signal([
-    {nome:'Teclado', preco:229.99},
-    {nome: 'Mouse', preco: 129.99},
-    {nome: 'Monitor', preco: 2000},
-    {nome: 'Desktop', preco: 4999.99},
-    {nome: 'Headset', preco: 500},
-  ]);
+  //lista com dados - Array
+  produtos = signal<{nome: string; preco: number}[]>([]);
+  carregando = signal (true);
   //!Função para exibir produtos selecionados pelo usuário no console
   exibirProduto (nome: string){
     console.log('Produto Selecionado: ', nome);
     this.produtoSelecionado.set(nome);
-  }
+  } 
   //! função que adiciona produto usando método update()
   adicionarProduto(){
     this.produtos.update(listaAtual => [
@@ -39,7 +36,8 @@ export class ListaProdutos {
   valorTotal = computed(() =>
   {return this.produtos().reduce((total, item) =>
   total + item.preco,0  
-  )});
+  )}
+);
 //!função que substitui a lista atual usando o método set ()
 substituirProduto(){
   this.produtos.set([
@@ -52,6 +50,9 @@ substituirProduto(){
 }
 //! método para monitorar alterações em tempo real usando effect ()
 constructor(){
+  
+  this.carregarProdutos();
+  
   effect(() =>{
     console.log('Lista de Produtos Alterados: ', this.produtos());
   });
@@ -70,17 +71,33 @@ produtoSelecionado = signal <string | null>(null);
 carrinho = signal <{nome: string; preco: number}[]>([]);
 adicionarAoCarrinho(produto:{nome: string; preco: number}){
   this.carrinho.update(listaAtual =>[
-    ...listaAtual,produto]
-  );
+    ...listaAtual,produto
+  ]);
 }
 //totalProduto = computed(() => this.produtos().length);
 //metodo para calcular a quantidade total de itens no carrinho
 quantidadeCarrinho = computed(() => this.carrinho().length);
-//metodo para calcular o valor total dos itens do carrinho
+//metodo para calcular o valor total dos itens do carrinho]
 totalCarrinho = computed(() => {
   return this.carrinho().reduce((total, item) =>
   total + item.preco,0)});
-//! valorTotal = computed(() => {
-//! return this.produtos().reduce((total, item) =>
-//! total + item.preco,0)});
+
+  carregarProdutos(){
+    this.carregando.set(true);
+    this.produtosService.buscarProdutos().subscribe({
+    next: (dados) =>{
+      const produtos = this.produtosService.transformarProdutos(dados);
+      this.produtos.set(produtos);
+      this.carregando.set(false);
+    },
+    error: (error) =>{
+      console.error('Erro ao carregar produtos', error);
+      this.carregando.set(false);
+    }
+  });
+
+  }
+
+  private produtosService = inject(produtosService);
+
 }
