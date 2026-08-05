02e15db2c1e782b54c7f7034d484fce42170c8b1
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-04 21:17:28 -0300

    refactor:src/app/features/produtos/produto/produto.ts

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -18,4 +18,16 @@ export class Produto {
   selecionarProduto(){
     this.produtoSelecionado.emit(this.nome);
   }
+
+  @Output() produtoAdicionado = new EventEmitter<{
+    nome: string;
+    preco: number;
+  }>();
+
+  adicionarAoCarrinho() {
+    this.produtoAdicionado.emit({
+      nome:this.nome,
+      preco:this.preco,
+    });
+  }
 }
