363c7fb1ecf5777bb6499d721f67129043cea433
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-05 21:53:09 -0300

    refactor: src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,19 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 import { usuarioLogado, login, logout } from './core/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { Header } from './chared/layout/header/header';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, Header, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Mercado Liso';
+  nomeLoja = 'Mercado do Sidney';
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
