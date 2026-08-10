8b1cb6ee35ca15bd3199fee8a0185817252553a2
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-10 20:41:42 -0300

    refactor: src/app/hared/layout/header/header.ts

--- src/app/hared/layout/header/header.ts
+++ src/app/hared/layout/header/header.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +12,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header {
-  nomeLoja = 'Mercado do Sidney'
+  nomeLoja = 'Mercado da Anny';
+  private carrinhoService = inject(CarrinhoService);
+  quantidadeHeader =this.carrinhoService.quantidadeitens;
 }
