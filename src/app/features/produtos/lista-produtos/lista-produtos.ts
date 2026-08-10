7fce69245f7a11b6af1c281c220ae75347ade4dd
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-10 20:39:25 -0300

    refactor:src/app/features/produtos/lista-produtos/lista-produtos.ts

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { Produto } from '../produto/produto';
-import { signal } from '@angular/core';
-import { computed } from '@angular/core';
-import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
-import { effect } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
-import { produtosService } from '../ptodutos.service';
-import { inject } from '@angular/core';
+
+import { Produto } from '../produto/produto';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { produtosService } from '../../../core/services/ptodutos.service';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -15,94 +13,130 @@ import { inject } from '@angular/core';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  //lista com dados - Array
-  produtos = signal<{nome: string; preco: number}[]>([]);
-  carregando = signal (true);
-  produtoselecionado = signal <string | null>(null);
-  erro = signal <string | null>(null);
-  
-  //!Função para exibir produtos selecionados pelo usuário no console
-  exibirProduto (nome: string){
-    console.log('Produto Selecionado: ', nome);
+
+  // Lista de produtos
+  produtos = signal<{ nome: string; preco: number }[]>([]);
+
+  // Estado de carregamento
+  carregando = signal(true);
+
+  // Produto selecionado
+  produtoSelecionado = signal<string | null>(null);
+
+  // Estado de erro
+  erro = signal<string | null>(null);
+
+  // Serviços
+  private produtosService = inject(produtosService);
+  public carrinhoService = inject(CarrinhoService);
+
+  // Quantidade e total do carrinho
+  quantidadeCarrinho = this.carrinhoService.quantidadeitens;
+  totalCarrinho = this.carrinhoService.totalitens;
+
+  // Quantidade de produtos
+  totalprodutos = computed(() => this.produtos().length);
+
+  // Valor total dos produtos
+  valorTotal = computed(() =>
+    this.produtos().reduce(
+      (total, item) => total + item.preco,
+      0
+    )
+  );
+
+  constructor() {
+
+    // Carrega os produtos
+    this.carregarProdutos();
+
+    // Monitora alterações na lista
+    effect(() => {
+      console.log(
+        'Lista de produtos alterados: ',
+        this.produtos()
+      );
+    });
+
+    // Monitora alteração do valor total
+    effect(() => {
+      console.log(
+        'Valor Total Atualizado: ',
+        this.valorTotal()
+      );
+    });
+
+    // Atualiza o título da página
+    effect(() => {
+      if (typeof document !== 'undefined') {
+        document.title = `(${this.totalprodutos()}) - Loja do Sidney`;
+      }
+    });
+  }
+
+  // Exibe produto selecionado
+  exibirProduto(nome: string) {
+    console.log('Produto selecionado:', nome);
     this.produtoSelecionado.set(nome);
-  } 
-  //! função que adiciona produto usando método update()
-  adicionarProduto(){
+  }
+
+  // Adiciona produto usando update()
+  adicionarproduto() {
     this.produtos.update(listaAtual => [
       ...listaAtual,
-      {nome:'Playstation 5', preco:3000},
+      {
+        nome: 'playstation 5',
+        preco: 3000
+      }
     ]);
   }
-  //! função que contabiliza a quantidade de produtos na lista
-  totalProdutos = computed(() => this.produtos().length);
-  //!função que calcula o valor total do produto usando o método cumputed()
-  valorTotal = computed(() =>
-  {return this.produtos().reduce((total, item) =>
-  total + item.preco,0  
-  )}
-);
-//!função que substitui a lista atual usando o método set ()
-substituirProduto(){
-  this.produtos.set([
-    {nome:'Teclado', preco:229.99},
-    {nome: 'Mouse', preco: 129.99},
-    {nome: 'Monitor', preco: 2000},
-    {nome: 'Desktop', preco: 4999.99},
-    {nome: 'Headset', preco: 500},
-  ]);
-}
-//! método para monitorar alterações em tempo real usando effect ()
-constructor(){
-  
-  this.carregarProdutos();
-  
-  effect(() =>{
-    console.log('Lista de Produtos Alterados: ', this.produtos());
-  });
-  effect(() =>{
-    console.log('Valor Total Atualizado: ', this.valorTotal());
-  });
-  effect(() =>{
-    if (typeof document !== 'undefined'){
-      document.title = `(${this.totalProdutos()}) - Loja do Sidney`;
-    }
-  });
-}
-//! Método para criar um estado de seleção com signal string | null
-produtoSelecionado = signal <string | null>(null);
-//! método para criar um estado para carrinho com signal
-carrinho = signal <{nome: string; preco: number}[]>([]);
-adicionarAoCarrinho(produto:{nome: string; preco: number}){
-  this.carrinho.update(listaAtual =>[
-    ...listaAtual,produto
-  ]);
-}
-//totalProduto = computed(() => this.produtos().length);
-//metodo para calcular a quantidade total de itens no carrinho
-quantidadeCarrinho = computed(() => this.carrinho().length);
-//metodo para calcular o valor total dos itens do carrinho]
-totalCarrinho = computed(() => {
-  return this.carrinho().reduce((total, item) =>
-  total + item.preco,0)});
 
-  carregarProdutos(){
-    this.erro.set(null);
-    this.carregando.set(true);
-    this.produtosService.buscarProdutos().subscribe({
-    next: (dados) =>{
-      const produtos = this.produtosService.transformarProdutos(dados);
-      this.produtos.set(produtos);
-      this.carregando.set(false);
-    },
-    error: (error) =>{
-      console.error('Erro ao carregar produtos', error);
-      this.erro.set('Erro ao carregar produtos. Por favor, tente novamente!');
-      this.carregando.set(false);
-    },
-  });
-
+  // Substitui a lista usando set()
+  substituirproduto() {
+    this.produtos.set([
+      { nome: 'teclado', preco: 50 },
+      { nome: 'mouse', preco: 15 },
+      { nome: 'monitor', preco: 500 },
+      { nome: 'desktop', preco: 1500 },
+      { nome: 'headset', preco: 30 },
+    ]);
   }
 
-  private produtosService = inject(produtosService);
+  // Adiciona produto ao carrinho
+  adicionarAoCarrinho(produto: {
+    nome: string;
+    preco: number;
+  }) {
+    this.carrinhoService.adicionar(produto);
+  }
 
+  // Carrega produtos
+  carregarProdutos() {
+    this.erro.set(null);
+    this.carregando.set(true);
+
+    this.produtosService.buscarProdutos().subscribe({
+      next: (dados) => {
+
+        const produtos =
+          this.produtosService.transformarProdutos(dados);
+
+        this.produtos.set(produtos);
+        this.carregando.set(false);
+      },
+
+      error: (erro) => {
+        console.error(
+          'Erro ao carregar produtos',
+          erro
+        );
+
+        this.erro.set(
+          'Erro ao carregar produtos. Por favor, tente novamente!'
+        );
+
+        this.carregando.set(false);
+      }
+    });
+  }
 }
