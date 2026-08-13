3e6b6e0ea2d2db1df2489572c1c968aa1b92ccbf
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-12 21:41:09 -0300

    refactor:  src/app/features/checkout/checkout/checkout.ts

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,18 +1,20 @@
-import { Component,inject } from '@angular/core';
+import { Component,inject, signal } from '@angular/core';
 import{ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 import { Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
-import { V } from '@angular/cdk/keycodes';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PrecoFormatadoPipe],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class Checkout {
 
   carrinhoService = inject(CarrinhoService);
+
+  compraFinalizada = signal(false);
 
   formulario = new FormGroup({
     nome: new FormControl('',[Validators.required,Validators.minLength(2), nomeSemNumeros]),
@@ -22,16 +24,31 @@ export class Checkout {
 
   finalizar(){
 
+    this.compraFinalizada.set(false);
+
+    if(this.carrinhoService.carrinhoVazio()){
+      console.log('Não é possível finalizar a compra com o carrinho vazio');
+      return;
+    }
+
     if(this.formulario.invalid){
-      console.log('Formulário Inválido');
+      console.log('Formulário Inválido!');
+      this.formulario.markAllAsTouched();
       return;
     }
 
     const dados = this.formulario.value;
     const itens = this.carrinhoService.itens();
+    const total = this.carrinhoService.totalitens();
 
+    console.log('Compra finalizada com sucesso!');
      console.log('Dados do Formulario: ', dados);
      console.log('Itens no carrinho: ', itens);
+     console.log('Total de compra: ', total);
+
+     this.carrinhoService.limpar();
+     this.formulario.reset();
+     this.compraFinalizada.set(true);
   }
 }
 function nomeSemNumeros(controle:AbstractControl):ValidationErrors | null {
