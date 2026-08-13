c24c1a584fcd57925e9a4c129e0c0c43ac73df42
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-12 21:40:15 -0300

    refactor: src/app/core/auth.ts

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -2,6 +2,11 @@ import { Injectable} from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+type ItemCarrinho ={
+    nome: string;
+    preco: number;
+}
+
 @Injectable({
     providedIn:'root'
 })
@@ -9,16 +14,17 @@ import { computed } from "@angular/core";
 export class CarrinhoService {
 
     //!estado global-criado com sucesso
-    private carrinho = signal<{nome: string; preco: number}[]>([]);
+    private carrinho = signal<ItemCarrinho[]>([]);
 
     //? seleções
     itens = computed(() => this.carrinho());
     quantidadeitens = computed(() => this.carrinho().length);
     totalitens = computed(() =>
     this.carrinho().reduce((total, item) => total + item.preco,0));
+    carrinhoVazio = computed(() => this.carrinho().length === 0);
 
     // TODO :AÇÕES
-adicionar(produto:{nome:string;preco:number}){
+adicionar(produto:ItemCarrinho){
     this.carrinho.update(lista =>[...lista, produto]);
 }
 // TODO: ação de limpeza
