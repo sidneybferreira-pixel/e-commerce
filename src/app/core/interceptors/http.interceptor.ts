74768235c25e3c2bf900845d8aafe103e1679145
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-13 21:21:07 -0300

    refactor:src/app/core/interceptors/http.interceptor.ts

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,33 +1,43 @@
-import { HttpInterceptorFn } from "@angular/common/http";
-import { tap } from "rxjs" ;
-import { catchError } from "rxjs";
-import { throwError } from "rxjs";
+import { HttpInterceptorFn } from '@angular/common/http';
+import { tap, catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
+  console.log('Interceptando requisição:', req.url);
 
-    console.log('Inteceptando Requisitação: ', req.url);
+  const authService = inject(AuthService);
+  const token = authService.obterToken();
 
-    //! Aqui você pode adicionar Lógica para
-    const token = 'fake-token-jwt';
-    const novaReq = req.clone({
+  const novaReq = token
+    ? req.clone({
         setHeaders: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-return next (novaReq). pipe(
-   tap({
-      next: (event) => console.log('Responde: ', event),
-      error: (error) => console.error('Erro de Requisitação: ', error)
+          Authorization: `Bearer ${token}`
+        }
+      })
+    : req;
+
+  return next(novaReq).pipe(
+    tap({
+      next: (event) => console.log('Resposta:', event),
+      error: (error) => console.error('Erro de requisição:', error)
     }),
-    catchError((error) =>{
-        console.error('ERRO GLOBAL:', error)
-        if (error.status === 401){
-            console.warn('Usuário não autorizado!');
-        }
-        if (error.status === 500){
-            console.warn('Erro interno do servidor!');
-        }
-        return throwError(() => error);
+
+    catchError((error) => {
+      console.error('ERRO GLOBAL:', error);
+
+      if (error.status === 401) {
+        console.warn(
+          'Erro de autenticação de usuário:',
+          error
+        );
+      }
+
+      if (error.status === 500) {
+        console.warn('Erro interno do servidor!');
+      }
+
+      return throwError(() => error);
     })
-   );
+  );
 };
