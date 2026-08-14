7357c812833aa80b238816340979113d1610fea2
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-13 21:19:59 -0300

    refactor:src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
-import { LowerCasePipe } from '@angular/common';
-import { usuarioLogado, login, logout } from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Header } from './hared/layout/header/header';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, MatCardModule, Header],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    UpperCasePipe,
+    MatButtonModule,
+    MatCardModule,
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+
   nomeLoja = 'Mercado do Sidney';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
+
+  authService = inject(AuthService);
+
+  usuarioLogado = this.authService.usuarioLogado;
+  login = this.authService.login.bind(this.authService);
+  logout = this.authService.logout.bind(this.authService);
 }
