c95fe37c31e7fac1c579b04e697d30c84c748e91
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-13 21:22:32 -0300

    refactor:src/app/features/login/login/login.ts

--- src/app/features/login/login/login.ts
+++ src/app/features/login/login/login.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  erroLogin = signal(false);
+
+  formulario = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  });
+
+  entrar() {
+
+    this.erroLogin.set(false);
+
+    if (this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+
+    const email = this.formulario.value.email ?? '';
+    const senha = this.formulario.value.senha ?? '';
+
+    const loginFinalizado = this.authService.login(email, senha);
+
+    if (!loginFinalizado) {
+      this.erroLogin.set(true);
+      return;
+    }
+
+    this.router.navigateByUrl('/produtos');
+  }
+}
