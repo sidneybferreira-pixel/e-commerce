8776e993b7afe8d4fe148e22c58bfee7e101d0ff
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-13 21:19:38 -0300

    refactor:src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,7 +26,13 @@ export const routes: Routes  = [
         loadComponent:() =>
             import('./features/checkout/checkout/checkout').then((m)=> m.Checkout)
     },
-     {
+    
+    {
+        path:'login',
+        loadComponent: () =>
+            import('./features/login/login/login').then((m) => m.Login),
+    },
+    {
         path:'**',
         redirectTo: '',
     },
