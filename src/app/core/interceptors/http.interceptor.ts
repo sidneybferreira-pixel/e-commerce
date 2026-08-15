ddc9408b32378c6f04467fc90e98de3591a8e99b
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-14 21:02:06 -0300

    refactor: src/app/core/interceptors/http.interceptor.ts

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -2,11 +2,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { tap, catchError, throwError } from 'rxjs';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('Interceptando requisição:', req.url);
 
   const authService = inject(AuthService);
+  const router = inject(Router);
   const token = authService.obterToken();
 
   const novaReq = token
@@ -31,13 +33,20 @@ export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
           'Erro de autenticação de usuário:',
           error
         );
+        authService.logout();
+        router.navigateByUrl('/login');
       }
 
       if (error.status === 500) {
         console.warn('Erro interno do servidor!');
       }
 
+      if(error.status === 403){
+        console.warn('Acesso Proibido, Usuário sem Permissão!');
+        router.navigateByUrl('/produtos');
+      }
+
       return throwError(() => error);
-    })
+    }),
   );
 };
