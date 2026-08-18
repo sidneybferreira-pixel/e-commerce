9c7fc9eae0a7c13f02dd3ba006433c391572ea8e
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-18 20:47:21 -0300

    refactor: src/app/features/admin/admin/admin.ts

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  totalProdutosCadastrados = signal(20);
+  pedidosPendentes = signal (6);
+  usuariosCadastrados = signal(7);
+
+  usuarioAtual = this.authService.usuarioAtual;
+
+  areaPerfil = computed(() =>{
+    const usuario = this.usuarioAtual();
+
+    if(!usuario){
+      return 'Nenhum usuário autenticado';
+    }
+    return `Usuário autenticado como: ${usuario.perfil}`;
+  });
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
