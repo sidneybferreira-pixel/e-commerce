78de37a1a6e9fe9892011ac409d65e1ded99913f
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-17 21:54:31 -0300

    refactor: src/app/features/acesso-negado/acesso-negado/acesso-negado.ts

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
-export class AcessoNegado {}
+export class AcessoNegado {
+
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    return;
+  }
+}
