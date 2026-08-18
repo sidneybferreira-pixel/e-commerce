7cea308ac189ca958712fbb06e8a6e30b1b89bf0
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-17 21:01:49 -0300

    refactor: src/app/features/acesso-negado/

---
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-acesso-negado',
+  imports: [RouterLink],
+  templateUrl: './acesso-negado.html',
+  styleUrl: './acesso-negado.css',
+})
+export class AcessoNegado {}
