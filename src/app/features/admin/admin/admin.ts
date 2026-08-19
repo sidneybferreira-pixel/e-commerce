a4c951613aee76eb4fb4312fd81c603ffeeca82c
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-19 19:48:23 -0300

    refactor: src/app/features/admin/admin/admin.ts

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,6 +1,6 @@
 import { Component, computed, signal, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../../core/services/auth.service';
+import { AuthFacade } from '../../../core/facades/auth.facade';
 
 @Component({
   selector: 'app-admin',
@@ -11,13 +11,13 @@ import { AuthService } from '../../../core/services/auth.service';
 export class Admin {
 
   private router = inject(Router);
-  private authService = inject(AuthService);
+  private authFacade = inject(AuthFacade);
 
   totalProdutosCadastrados = signal(20);
   pedidosPendentes = signal (6);
   usuariosCadastrados = signal(7);
 
-  usuarioAtual = this.authService.usuarioAtual;
+  usuarioAtual = this.authFacade.usuarioAtual;
 
   areaPerfil = computed(() =>{
     const usuario = this.usuarioAtual();
@@ -29,7 +29,7 @@ export class Admin {
   });
 
   sair(){
-    this.authService.logout();
+    this.authFacade.sair();
     this.router.navigateByUrl('/login');
   }
 }
