3b3960f276db54d808e3285aee031e32c3c3f5d9
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-19 19:46:59 -0300

    refactor: src/app/core/auth.guard.ts

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,14 +1,14 @@
 import { CanActivateFn } from "@angular/router";
-import { AuthService } from "./services/auth.service";
+import { AuthFacade } from "./facades/auth.facade";
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 
     export const authGuard: CanActivateFn = () => {
         
-        const authService = inject(AuthService);
+        const authFacade = inject(AuthFacade);
         const router = inject(Router);
 
-        if (authService.usuarioLogado()){
+        if (authFacade.usuarioLogado()){
             return true;
         }
         return router.createUrlTree(['/login']);
