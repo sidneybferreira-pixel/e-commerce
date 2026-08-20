ffb51115f146d205da9aadf6c5341b8d3093bd78
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-20 20:02:58 -0300

    refactor: src/app/core/facades/auth.facade.ts

--- src/app/core/facades/auth.facade.ts
+++ src/app/core/facades/auth.facade.ts
@@ -1,31 +1,27 @@
-import { Injectable, inject } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { Injectable, inject } from "@angular/core";
+import { AuthService } from "../services/auth.service";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
+
 export class AuthFacade {
 
-  private authService = inject(AuthService);
+    private authService = inject(AuthService);
 
-  usuarioAtual = this.authService.usuarioAtual;
-  usuarioLogado = this.authService.usuarioLogado;
-  admin = this.authService.admin;
-  token = this.authService.token;
+    usuarioAtual = this.authService.usuarioAtual;
+    usuarioLogado = this.authService.usuarioLogado;
+    admin = this.authService.admin;
+    token = this.authService.token;
 
-  realizarLogin(email: string, senha: string): boolean {
-    return this.authService.login(email, senha);
-  }
-
-  sair(): void {
-    this.authService.admin();
-  }
-
-  obterToken() {
-    return this.authService.obterToken();
-  }
-
-  obterPerfil() {
-    return this.authService.obterPerfil();
-  }
+    realizarLogin(email: string, senha: string):boolean{
+        return this.authService.login(email,senha);
+    }
+    sair(){
+        this.authService.logout();
+    }
+    obterToken(){
+        return this.authService.obterToken();
+    }
+    obterPerfil(){
+        return this.authService.obterPerfil();
+    }
 }
