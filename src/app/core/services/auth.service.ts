45549c29a6cc952aba3ddc4de6110a9ac46c8f7e
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-20 20:13:41 -0300

    refactor: src/app/core/services/auth.service.ts

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   usuarioAtual = computed(() => this.usuario());
   usuarioLogado = computed(() => this.usuario() !== null);
   token = computed(() => this.tokenJwt());
-  admin = computed(() => this.usuario()?.perfil === 'admin');
+  admin = computed(() => this.usuario()?.perfil ==='admin');
 
   login(email: string, senha: string): boolean {
 
@@ -26,7 +26,7 @@ export class AuthService {
       return false;
     }
 
-    const perfil: PerfilUsuario = email === 'admin@email.com.br' ? 'admin' : 'usuario';
+    const perfil: PerfilUsuario = email === 'admin@email.com.br'? 'admin' : 'usuario'
     const tokenSimulado =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
       'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' +
