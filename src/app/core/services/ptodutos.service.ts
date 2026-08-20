1f469517bafe6bd6a014b65dcdd74a1a02f2c18f
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-20 20:14:54 -0300

    refactor:   src/app/core/services/ptodutos.service.ts

--- src/app/core/services/ptodutos.service.ts
+++ src/app/core/services/ptodutos.service.ts
@@ -1,32 +1,30 @@
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable} from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-type ProdutoApi = {
+type produtoApi = {
 
     title: string;
     price: number;
 };
-type Produto = {
+type produto ={
 
     nome: string;
     preco: number;
 };
 
-@Injectable({providedIn: 'root'})
+@Injectable ({providedIn: 'root'})
 
 export class produtosService{
-
     private http = inject(HttpClient);
     private API = 'https://fakestoreapi.com/products';
 
-buscarProdutos(){
-    return this.http.get<ProdutoApi[]>(this.API);
+    buscarProdutos(){
+        return this.http.get<produtoApi[]>(this.API);
     }
-
-transformarProdutos(dados: ProdutoApi[]): Produto[] {
-    return dados.map((p) => ({
-        nome: p.title,
-        preco: p.price,
-    }));
-}
+    TransformarProdutos(dados: produtoApi[]):produto[] {
+        return dados.map((p) => ({
+            nome: p.title,
+            preco: p.price,
+        }));
+    }
 }
