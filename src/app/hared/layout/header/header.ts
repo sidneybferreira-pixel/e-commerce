87802e10cd7433ead0d8ea7df6e28afd925fd930
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-21 19:53:21 -0300

    refactor:   src/app/hared/layout/header/header.ts

--- src/app/hared/layout/header/header.ts
+++ src/app/hared/layout/header/header.ts
@@ -1,29 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, Router } from '@angular/router';
-import { inject } from '@angular/core';
+
 import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
 import { AuthFacade } from '../../../core/facades/auth.facade';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, RouterLink],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+
   nomeLoja = 'Mercado do Sidney';
+
   private carrinhoFacade = inject(CarrinhoFacade);
-  quantidadeHeader =this.carrinhoFacade.quantidadeCarrinho;
+  quantidadeHeader = this.carrinhoFacade.quantidadeCarrinho;
 
   private authFacade = inject(AuthFacade);
+
   usuarioLogado = this.authFacade.usuarioLogado;
   usuarioAtual = this.authFacade.usuarioAtual;
 
   private router = inject(Router);
 
-  sair(){
+  sair() {
     this.authFacade.sair();
     this.router.navigateByUrl('/login');
   }
