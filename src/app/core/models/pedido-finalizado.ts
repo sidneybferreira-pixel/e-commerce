9aa02eaa65fbcbd887227bf61b29a7f05c14c383
Sidney Brito <Sidneyb09ferreira@gmail.com>
2026-08-21 20:30:28 -0300

    refactor:  src/app/core/models/pedido-finalizado.ts

--- src/app/core/models/pedido-finalizado.ts
+++ src/app/core/models/pedido-finalizado.ts
@@ -1,9 +1,10 @@
 import { ItemCarrinho } from "./item-carrinho";
 
-export type PedidoFinalizado ={
-    codigo: number;
-    cliente: string;
-    quantidadeItens: number;
-    total: number;
-    itens: ItemCarrinho;
-}
+export type PedidoFinalizado = {
+  codigo: number;
+  cliente: string;
+  email: string;
+  quantidadeItens: number;
+  total: number;
+  itens: ItemCarrinho[];
+};
